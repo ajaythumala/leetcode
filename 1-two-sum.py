18ce1007af2285f24e6205d6cca969712add03e5
ajaythumala <87052780+ajaythumala@users.noreply.github.com>
2022-05-22 02:43:37 +0530

    Update 1-two-sum.py

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -8,3 +8,10 @@ class Solution:
                 return dictionary[target-nums[index]], index
         
         
+        #alternate method: 22.05.22 - using enumerate function
+        occurs = {}
+        for index, num in enumerate(nums):
+            occurs[num] = occurs.get(num, index)
+            if target - num in occurs and index != occurs[target - num]:
+                return occurs[target - num], index
+        
