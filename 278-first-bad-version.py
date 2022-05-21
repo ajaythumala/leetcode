e503c549fa3d8acd0bca59ddc5a74e610926c17b
ajaythumala <87052780+ajaythumala@users.noreply.github.com>
2022-05-21 19:23:25 +0530

    Update 278-first-bad-version.py

--- 278-first-bad-version.py
+++ 278-first-bad-version.py
@@ -3,19 +3,24 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        
         (left, right) = (1, n)
-        
-        while left <= right:
-            pivot = left + ((right - left) // 2)
-            
-            if isBadVersion(pivot) == True:
-                if isBadVersion(pivot - 1) == False or (pivot - 1) < 0:
-                    return pivot
-                else:
-                    right = pivot - 1
-                    
+        while left < right:
+            pivot = (left + right) // 2 # could overflow in other languages : use pivot = left + ((right - left) // 2) if needed
+            if isBadVersion(pivot):
+                right = pivot
             else:
                 left = pivot + 1
+        return left #left, not pivot as it fails in the case of there being only 1 element.
+                    #we assume this because there is always one bad version in the input.
+                    #verified as the submission was accepted.
+              
+#         while left <= right:
+#             pivot = (left + right) // 2
+#             if isBadVersion(pivot) == True:
+#                 if isBadVersion(pivot - 1) == False or (pivot - 1) < 0:
+#                     return pivot
+#                 else:
+#                     right = pivot - 1
+
         
         
